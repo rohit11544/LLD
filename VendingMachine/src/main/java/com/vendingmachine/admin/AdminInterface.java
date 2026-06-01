9db38fe6f8716eb6b40399e9e9b6a41588e07e8c
Rohit11544 <borra.rohit@fynmobility.com>
2026-06-01 20:23:07 +0530

    Add files via upload

--- VendingMachine/src/main/java/com/vendingmachine/admin/AdminInterface.java
+++ VendingMachine/src/main/java/com/vendingmachine/admin/AdminInterface.java
@@ -1,1 +1,9 @@
+package com.vendingmachine.admin;
 
+public interface AdminInterface {
+    void refillItem(String itemCode, int quantity);
+    void collectCash(double amount);
+    void addItem(String code, String name, double price, int quantity);
+    void viewInventory();
+    void viewCashCollected();
+}
