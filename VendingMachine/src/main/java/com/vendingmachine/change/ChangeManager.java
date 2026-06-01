caf33fc3853a5291e3a5c0fc988965215fefd44d
Rohit11544 <borra.rohit@fynmobility.com>
2026-06-01 20:26:37 +0530

    Add files via upload

--- VendingMachine/src/main/java/com/vendingmachine/change/ChangeManager.java
+++ VendingMachine/src/main/java/com/vendingmachine/change/ChangeManager.java
@@ -1,1 +1,35 @@
+package com.vendingmachine.change;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChangeManager {
+    private static final int[] DENOMINATIONS = {50, 20, 10, 5, 1};
+
+    public Map<Integer, Integer> calculateChange(double change) {
+        int cents = (int) Math.round(change * 100);
+        Map<Integer, Integer> coinsDispensed = new HashMap<>();
+
+        for (int denom : DENOMINATIONS) {
+            int count = cents / denom;
+            if (count > 0) {
+                coinsDispensed.put(denom, count);
+                cents %= denom;
+            }
+        }
+
+        return coinsDispensed;
+    }
+
+    public void dispenseChange(double change) {
+        if (change <= 0) {
+            return;
+        }
+
+        Map<Integer, Integer> coinsDispensed = calculateChange(change);
+        System.out.println("Dispensing change: $" + String.format("%.2f", change));
+        coinsDispensed.forEach((denom, count) ->
+                System.out.println("  " + count + " coin(s) of " + denom + " cent(s)")
+        );
+    }
+}
